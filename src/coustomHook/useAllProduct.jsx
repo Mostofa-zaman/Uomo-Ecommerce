d4a04c8c55066042ed86e0c4c76e399e6b0baaa0
mostofa zaman <mostofazaman1234@gmail.com>
2026-04-30 21:04:40 +0600

    refactor(cart): update store access to selector pattern and fix import path

--- src/coustomHook/useAllProduct.jsx
+++ src/coustomHook/useAllProduct.jsx
@@ -1,6 +1,5 @@
 import { allProuct } from "@/api/apiInstans";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
 
 const useAllProduct = () => {
   return useQuery({ queryKey: ["all-products"], queryFn: allProuct });
